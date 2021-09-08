d86641306211b10086b0059af9d185679f3b96ec
Aine Mota <motaaine1@gmail.com>
2021-09-08 17:03:24 -0300

    adicionei os elementos necessários no html, editei o js como solicitado e criei o css para melhorar a aparência

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+let frequency = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+function rolling(){
+    let dado = Math.floor((Math.random() * 6) + 1)
+    return dado;
+}
+
+const som = () => {
+    let dado1 = rolling();
+    let dado2 = rolling();
+    let soma = dado1 + dado2;
+
+    frequency[soma] = frequency[soma] + 1;
+
+    return soma;
+}
+
+for(let i = 0; i < 1000; i++){
+    som();
+}
+
+
+const numbersResult = document.getElementById('numbersResult');
+
+for( let i = 2; i <= 12; i++){
+    const p = document.createElement('p');
+
+    p.innerText = i + ' : ' + frequency[i];
+
+    numbersResult.appendChild(p);    
+}
+
+const randomColor = () => {
+    const colors = ["#1F271B", "#003F91", "#6D326D", "#157A6E", "#916953", "#000", "#836FFF","#708090","#008080","#00FA9A","#8B4513", "#BC8F8F",];
+  
+    return colors[Math.floor(Math.random() * 12)];
+  };
+
+const barResult = document.getElementById('barResult')
+
+for( let i = 2; i <= 12; i++){
+    const bar = document.createElement('div');
+
+    bar.style.width = `${frequency[i] / 2 }vw`;
+    bar.style.background = randomColor();
+    bar.style.color = `#fff`;
+    bar.innerText = i;
+
+    numbersResult.appendChild(bar);    
+}
